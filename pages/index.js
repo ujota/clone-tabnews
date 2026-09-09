ec69f3246ee587a727375cd73b2f0348b635b354
JOTA <63990197+ujota@users.noreply.github.com>
2026-09-09 01:04:05 +0000

    adicionar mundanças na pagina index

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,4 @@ function Home() {
   return <h1>Minha vida eu te amo! Se você me ama me manda um beijinho!?</h1>;
 }
 
-function teste() {
-  console.log("identação errada");
-}
-
 export default Home;
